5299e29164f8fab4cb207c828f948166095356c7
Marco <marco.reimann@direkt-gruppe.de>
2019-06-21 10:54:14 +0200

    init project

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+const autoprefixer = require('autoprefixer');
+const tailwind = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+      './src/**/*.html',
+      './src/**/*.vue',
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = {
+    plugins: [
+        autoprefixer(),
+        tailwind('tailwind.config.js'),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
+    ]
+}
