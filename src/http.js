0e42f1e76a686af0b2e74f45466376051a6f7d65
Reimann <marco.reimann@direkt-gruppe.de>
2019-08-20 09:45:59 +0200

    added memes

--- src/http.js
+++ src/http.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import nprogress from 'nprogress';
 
+// Memes
+// https://send.firefox.com/download/540555943cfcabcc/#Ss4jlPBCm5btC4ZWeSWiMA
+
 let instance = axios.create({
-    baseURL: 'https://api.secrethitler.tk/api'
+    baseURL: 'https://51.68.188.114/api'
 });
 
 instance.interceptors.request.use(config => {
