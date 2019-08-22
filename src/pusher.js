496791848c353f73c6dc6bb90a9e6a877813527d
Reimann <marco.reimann@direkt-gruppe.de>
2019-08-22 11:11:15 +0200

    IT WORKS!!!!

--- src/pusher.js
+++ src/pusher.js
@@ -1,8 +1,23 @@
 import Pusher from 'pusher-js';
+import http from './http';
 
 let pusher = new Pusher('dbaa9e20ac7717618b2a', {
     authEndpoint: 'https://api.secrethitler.tk/api/pusher/auth',
     cluster: 'eu',
+    authorizer: ({ name }) => ({
+        authorize: async (socketId, callback) => {
+            try {
+                let res = await http.post('/pusher/auth', {
+                    socket_id: socketId,
+                    channel_name: name
+                });
+
+                callback(false, res.data);
+            } catch(err) {
+                callback(true, null);
+            }
+        },
+    }),
     // forceTLS: true,
 });
 
