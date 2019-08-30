a91b571ab7a35fb03095afbae2c31b664397b584
Reimann <marco.reimann@direkt-gruppe.de>
2019-08-30 12:35:01 +0200

    change secret hitler

--- src/components/ShowRole.vue
+++ src/components/ShowRole.vue
@@ -15,7 +15,7 @@
             <div class="flex justify-center">
                 <img v-if="role == 'fascist'" src="../assets/role_fascist.jpg" alt="Fascist">
                 <img v-if="role == 'liberal'" src="../assets/role_liberal.jpg" alt="Liberal">
-                <img v-if="role == 'hitler'" src="../assets/role_hitler.jpg" alt="Hitler">
+                <img v-if="role == 'secret hitler'" src="../assets/role_hitler.jpg" alt="Hitler">
             </div>
         </modal>
     </div>
