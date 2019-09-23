272ea62661c0319b19b7594ad820d8573b93c1fa
Reimann <marco.reimann@direkt-gruppe.de>
2019-09-23 09:22:37 +0200

    convert user id to int

--- src/views/GameKillPlayer.vue
+++ src/views/GameKillPlayer.vue
@@ -56,7 +56,7 @@ export default {
             this.eliminating = true;
             this.$http.post('/player/execute', {
                 channelName: this.$route.params.id,
-                userId: id,
+                userId: parseInt(id),
             })
             .then(res => {
                 this.$router.push({name: 'info', params: { id: this.$route.params.id}});
